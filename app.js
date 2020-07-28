389ba72b6fe91e11bc9eec9ddd80613bf6d4ddb7
nikitha26 <mnikitha365@gmail.com>
2020-07-28 12:17:17 +0530

    string contain number or not

---
+++ app.js
@@ -0,0 +1,12 @@
+//In given string numerical numbers is occured or not .
+//occured any number return true otherwise return false.
+let x = "nikitha"
+var reg = x.match(/\d/);
+if(reg != null)
+	{
+		console.log(true);
+	}
+ else
+	 {
+		 console.log(false);
+	 }
